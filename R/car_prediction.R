e0d1567acb702c7b7875d6feb2cdeb94049fe048
Danny Volkaerts <u0073950@ucll.be>
2019-06-11 16:24:40 +0200

    no colname 'speed' necessary

--- R/car_prediction.R
+++ R/car_prediction.R
@@ -1,15 +1,15 @@
 car <- function(input){
   library(mgcv)
-  #input can either be csv file or data	
+  #input can either be csv file or data
   newdata <- if(is.character(input) && file.exists(input)){
     read.csv(input)
   } else {
     as.data.frame(input)
   }
-  stopifnot("speed" %in% names(newdata))
-  
-  newdata$speed <- as.numeric(newdata$speed)
-  
+  #stopifnot("speed" %in% names(newdata))
+
+  newdata$speed <- as.numeric(newdata[,1])
+
   #tv_model is included with the package
   newdata$dist <- predict.gam(car_model, newdata = newdata)
   return(newdata)
